9b01564551e2807193e0720bda24a101a83c9292
NandishDPatel <nandishdpatel22@gmail.com>
2024-09-17 07:46:44 -0500

    Basic layout for the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import LandingPage from './components/custom/LandingPage'
 import { Button } from './components/ui/button'
+import CreateTrip from './pages/CreateTrip'
 
 function App() {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import CreateTrip from './pages/createTrip.jsx'
+import CreateTrip from './pages/CreateTrip.jsx'
 import Header from './components/custom/Header.jsx'
 
 const router = createBrowserRouter([{
